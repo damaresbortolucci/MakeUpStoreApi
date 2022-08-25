e334ea3fc1135eaf93f2e8b406c53cdf5310fb09
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2022-08-25 04:07:05 -0300

    add gitignore

---
+++ MakeupStoreApi/Dtos/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MakeupStoreApi.Dtos
+{
+    public class ProductDto
+    {
+        public string brand { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+        public string image_link { get; set; }
+        public string description { get; set; }
+        public float rating { get; set; }
+        public string category { get; set; }
+        public string product_type { get; set; }
+        public string[] tag_list { get; set; }
+    }
+}
---
+++ MakeupStoreApi/Repository/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using MakeupStoreApi.Models;
+
+namespace MakeupStoreApi.Repository
+{
+    public static class ProductRepository
+    {
+        public static List<Product> GetData()
+        {
+            var reader = new StreamReader("./data.json");
+            var json = reader.ReadToEnd();
+            reader.Dispose();
+            var data = JsonSerializer.Deserialize<List<Product>>(json);
+            return data;
+        }
+    }
+}
